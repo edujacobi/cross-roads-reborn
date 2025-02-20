6432cb14376752504a2b42cc41f2ae4a20893d36
Eduardo Jacobi <dudu.jacobi@gmail.com>
2025-02-20 10:41:16 -0300

    feat: New CR Colors for Police and Robbery

--- utils/colors.ts
+++ utils/colors.ts
@@ -2,10 +2,12 @@ import { Colors } from "discord.js";
 
 export const CrColors = {
 	Default: Colors.Green,
-	Police: 0x273870,
+	Police: 0x163cb6,
+	Prison: 0x273870,
 	Casino: 0xffebd6,
 	Jobs: Colors.Yellow,
-	Robbery: Colors.DarkRed,
+	Robbery: 0xb43627,
 	BlackMarket: 0x000001,
 	Admin: 0x80e893,
+	Jacobi: 0x426b69,
 };
