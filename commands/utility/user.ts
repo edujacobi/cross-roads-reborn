47acdd7f191fbf8de907fdac5f47fe9eb9722e41
Eduardo Jacobi <dudu.jacobi@gmail.com>
2025-03-01 10:34:05 -0300

    feat: user command localization

--- commands/utility/user.ts
+++ commands/utility/user.ts
@@ -49,68 +49,76 @@ module.exports = {
 			// .setThumbnail(_user.avatarURL())
 			.setColor(Colors.DarkButNotBlack)
 			.setAuthor({
-				name: `Informações de ${target.Nickname}`,
+				name: `${s.title} ${target.Nickname}`,
 				iconURL: _user.avatarURL() ?? undefined,
 			})
 			.setDescription(`\n${badges.length > 0 ? `### ${badgeText}\n` : ""} ### ${formatMoney(target.Money, language)}`)
 			.setFields([{
-				name: "Situação",
+				name: s.situation,
 				value: `-# ${target.Situation.Complex}`,
 				inline: true,
 			}, {
-				name: "Classe",
+				name: s.className,
 				value: `-# ${ClassList[target.Class].Image.Emote.String} ${ClassList[target.Class].Description[user.Language]}`,
 				inline: true,
-			}, {
-				name: `${EmoteString.InvestmentActive} Investimento`,
-				value: `-# Não implementado`,
-				inline: true,
-			}, {
+			},
+			// {
+			// 	name: `${EmoteString.InvestmentActive} Investimento`,
+			// 	value: `-# Não implementado`,
+			// 	inline: true,
+			// },
+			{
 				name: `${EmoteString.Heads} Daily`,
-				value: `-# ${target.CanReceiveDaily() ? `Disponível` : showTime(addDays(target.Daily.LastReceived!, 1).getTime(), true)}`,
+				value: `-# ${target.CanReceiveDaily() ? s.available : showTime(addDays(target.Daily.LastReceived!, 1).getTime(), true)}`,
 				inline: true,
 			}, {
-				name: `${EmoteString.Prison} Prisão`,
-				value: `-# \`${target.Robbery.FailureCount}\` vezes preso
--# \`${target.Escape.Count}\` fugiu
--# \`${formatMoney(target.Prison.BriberySum, user.Language)}\` (\`${target.Prison.BriberyCount}\`) em suborno`,
+				name: `${EmoteString.Prison} ${s.prison}`,
+				value: `-# \`${target.Robbery.FailureCount}\` ${s.timesInPrison}
+-# \`${target.Escape.Count}\` ${s.escapes}
+-# \`${formatMoney(target.Prison.BriberySum, user.Language)}\` (\`${target.Prison.BriberyCount}\`) ${s.inBribery}`,
 				inline: true,
 			}, {
-				name: `${EmoteString.Robbery} Roubos`,
-				value: `-# ${target.Wanted.Time > now ? showTime(target.Wanted.Time.getTime(), true) : `Pode roubar`}
--# \`${formatMoney(target.Robbery.SuccessRobbedSum, user.Language)}\` (\`${target.Robbery.SuccessCount}\`) roubados
--# \`${formatMoney(target.Robbery.BeingRobbedSum, user.Language)}\` (\`${target.Robbery.BeingRobbedCount}\`) perdidos`,
+				name: `${EmoteString.Robbery} ${s.robberies}`,
+				value: `-# ${target.Wanted.Time > now ? showTime(target.Wanted.Time.getTime(), true) : s.canRob}
+-# \`${formatMoney(target.Robbery.SuccessRobbedSum, user.Language)}\` (\`${target.Robbery.SuccessCount}\`) ${s.robbed}
+-# \`${formatMoney(target.Robbery.BeingRobbedSum, user.Language)}\` (\`${target.Robbery.BeingRobbedCount}\`) ${s.robLost}`,
+				inline: true,
+			},
+			// 	{
+			// 	name: `${EmoteString.Beat} Espancamentos`,
+			// 	value: `-# Não implementado`,
+			// 	inline: true,
+			// },
+			{
+				name: `${EmoteString.Bank} ${s.money}`,
+				value: `-# \`${formatMoney(target.Job.ReceivedSum, user.Language)}\` (\`${target.Job.ReceivedCount}\`) ${s.fromJobs}
+-# \`${formatMoney(target.Shop.SpentSum, user.Language)}\` (\`${target.Shop.SpentCount}\`) ${s.spent}`,
 				inline: true,
 			}, {
-				name: `${EmoteString.Beat} Espancamentos`,
-				value: `-# Não implementado`,
-				inline: true,
-			}, {
-				name: `${EmoteString.Bank} Dinheiro`,
-				value: `-# \`${formatMoney(target.Job.ReceivedSum, user.Language)}\` (\`${target.Job.ReceivedCount}\`) de trabalhos
--# \`${formatMoney(target.Shop.SpentSum, user.Language)}\` (\`${target.Shop.SpentCount}\`) gastos em lojas`,
-				inline: true,
-			}, {
-				name: `${EmoteString.Casino} Cassino`,
-				value: `-# \`${target.Casino.WinCount + target.Casino.LoseCount}\` jogos
--# \`${formatMoney(target.Casino.WinSum, user.Language)}\` (\`${target.Casino.WinCount}\`) ganhos
--# \`${formatMoney(target.Casino.LoseSum, user.Language)}\` (\`${target.Casino.LoseCount}\`) perdidos
+				name: `${EmoteString.Casino} ${s.casino}`,
+				value: `-# \`${target.Casino.WinCount + target.Casino.LoseCount}\` ${s.games}
+-# \`${formatMoney(target.Casino.WinSum, user.Language)}\` (\`${target.Casino.WinCount}\`) ${s.won}
+-# \`${formatMoney(target.Casino.LoseSum, user.Language)}\` (\`${target.Casino.LoseCount}\`) ${s.lost}
 -# \`${(target.Casino.WinCount / (target.Casino.LoseCount) * 100).toFixed(2)}%\` win rate`,
 				inline: true,
-			}, {
-				name: `${EmoteString.Philantrope} Esmolas`,
-				value: `-# Não implementado`,
-				inline: true,
-			}, {
-				name: `${EmoteString.Hospital} Hospital`,
-				value: `-# Não implementado`,
-				inline: true,
-			}, {
-				name: `${EmoteString.Scavenge} Vasculhar`,
-				value: `-# Não implementado`,
-				inline: true,
-			}])
-			.setDefaultFooter(user.Nickname, interaction.user.avatarURL(), `ID: ${target.Id} • Jogando desde: ${target.CreatedAt.toLocaleDateString(interaction.locale)}`);
+			},
+				// {
+				// 	name: `${EmoteString.Philantrope} Esmolas`,
+				// 	value: `-# Não implementado`,
+				// 	inline: true,
+				// },
+				// {
+				// 	name: `${EmoteString.Hospital} Hospital`,
+				// 	value: `-# Não implementado`,
+				// 	inline: true,
+				// },
+				// {
+				// 	name: `${EmoteString.Scavenge} Vasculhar`,
+				// 	value: `-# Não implementado`,
+				// 	inline: true,
+				// },
+			])
+			.setDefaultFooter(user.Nickname, interaction.user.avatarURL(), `ID: ${target.Id} • ${s.playingSince}: ${target.CreatedAt.toLocaleDateString(interaction.locale)}`);
 
 		await replyInteraction(interaction, { embeds: [embed] });
 	},
@@ -118,29 +126,71 @@ module.exports = {
 
 const Strings = {
 	[Language.English]: {
-		title: "About",
-		credits: "Credits",
-		direction: "Direction",
-		programming: "Programming",
-		art: "Art",
-		disclaimer: "Some images were made using Image Creator from Microsoft Designer (Bing).",
+		title: "Informations of",
+		situation: "Situation",
+		className: "Class",
+		available: "Available",
+		prison: "Prison",
+		timesInPrison: "times in prison",
+		escapes: "escapes",
+		inBribery: "in bribery",
+		robberies: "Robberies",
+		canRob: "Can rob",
+		robbed: "robbed",
+		robLost: "lost",
+		money: "Money",
+		fromJobs: "from jobs",
+		spent: "spent in shops",
+		casino: "Casino",
+		games: "games",
+		won: "won",
+		lost: "lost",
+		playingSince: "Playing since",
 	},
 
 	[Language.Portuguese]: {
-		title: "Sobre",
-		credits: "Créditos",
-		direction: "Direção",
-		programming: "Programação",
-		art: "Arte",
-		disclaimer: "Algumas imagens foram criadas utilizando o Image Creator do Microsoft Designer (Bing).",
+		title: "Informações de",
+		situation: "Situação",
+		className: "Classe",
+		available: "Disponível",
+		prison: "Prisão",
+		timesInPrison: "vezes preso",
+		escapes: "fugas",
+		inBribery: "em suborno",
+		robberies: "Roubos",
+		canRob: "Pode roubar",
+		robbed: "roubados",
+		robLost: "perdidos",
+		money: "Dinheiro",
+		fromJobs: "de trabalhos",
+		spent: "gastos em lojas",
+		casino: "Cassino",
+		games: "jogos",
+		won: "ganhos",
+		lost: "perdidos",
+		playingSince: "Jogando desde",
 	},
 
 	[Language.Spanish]: {
-		title: "Acerca de",
-		credits: "Créditos",
-		direction: "Dirección",
-		programming: "Programación",
-		art: "Arte",
-		disclaimer: "Algunas imágenes se crearon utilizando Image Creator de Microsoft Designer (Bing).",
+		title: "Informaciones de",
+		situation: "Situación",
+		className: "Clase",
+		available: "Disponible",
+		prison: "Prisión",
+		timesInPrison: "veces en prisión",
+		escapes: "fugas",
+		inBribery: "en soborno",
+		robberies: "Robos",
+		canRob: "Puede robar",
+		robbed: "robados",
+		robLost: "perdidos",
+		money: "Dinero",
+		fromJobs: "de trabajos",
+		spent: "gastos en tiendas",
+		casino: "Casino",
+		games: "juegos",
+		won: "ganados",
+		lost: "perdidos",
+		playingSince: "Jugando desde",
 	},
 } as const;
