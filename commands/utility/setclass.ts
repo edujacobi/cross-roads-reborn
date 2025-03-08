1a96cd45e163d4179e0ec7fc8602fcbb7bb1858f
Eduardo Jacobi <dudu.jacobi@gmail.com>
2025-03-08 18:30:28 -0300

    fix: Setclass vip

--- commands/utility/setclass.ts
+++ commands/utility/setclass.ts
@@ -12,7 +12,6 @@ import { Language } from "../../models/Language";
 import { ClassId, ClassList } from "../../models/Class";
 
 module.exports = {
-	vip: true,
 	data: new SlashCommandBuilder()
 		.setName("setclass")
 		.setNameLocalization(Locale.PortugueseBR, "mudaclasse")
