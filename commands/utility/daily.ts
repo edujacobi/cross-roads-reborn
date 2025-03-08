da6df381c13ff1db319289adb17ffc7f9017498f
Eduardo Jacobi <dudu.jacobi@gmail.com>
2025-03-08 16:18:15 -0300

    feat: update Daily texts

--- commands/utility/daily.ts
+++ commands/utility/daily.ts
@@ -24,7 +24,7 @@ module.exports = {
 			const embed = defaultEmbed({
 				nickname: user.Nickname,
 				interaction,
-				description: s.descriptionReceived(user.Nickname, showTime(addDays(user.Daily.LastReceived, 1).getTime(), true)),
+				description: s.descriptionReceived(showTime(addDays(user.Daily.LastReceived, 1).getTime(), true)),
 				thumbnail: interaction.user.avatarURL() ?? "",
 				color: CrColors.Default,
 			});
@@ -37,7 +37,7 @@ module.exports = {
 		const embed = defaultEmbed({
 			nickname: user.Nickname,
 			interaction,
-			description: s.description(user, money),
+			description: s.description(money, user.Daily.CurrentStreak),
 			footer: s.footer(user.Daily.MaxStreak),
 			thumbnail: interaction.user.avatarURL() ?? "",
 			color: CrColors.Default,
@@ -49,19 +49,19 @@ module.exports = {
 
 const Strings = {
 	[Language.English]: {
-		descriptionReceived: (nickname: string, userLastDaily: string) => `**${nickname}** already receive his daily money in the last 24 hours.\n-# You will be able to receive again ${userLastDaily}!`,
-		description: (user: User, money: number) => `**${user.Nickname}** received ${formatMoney(money, user.Language)}.\n-# Your current daily streak is **${user.Daily.CurrentStreak}**.`,
+		descriptionReceived: (userLastDaily: string) => `You already receive his daily money in the last 24 hours.\n-# Will be able to receive again ${userLastDaily}!`,
+		description: (money: number, currentStreak: number) => `You received ${formatMoney(money, Language.English)}.\n-# Your current daily streak is **${currentStreak}**.`,
 		footer: (maxStreak: number) => `Max daily streak: ${maxStreak}`,
 	},
 
 	[Language.Portuguese]: {
-		descriptionReceived: (nickname: string, userLastDaily: string) => `**${nickname}** já recebeu sua grana diária nas últimas 24 horas.\n-# Você poderá receber novamente ${userLastDaily}!`,
-		description: (user: User, money: number) => `**${user.Nickname}** recebeu ${formatMoney(money, user.Language)}.\n-# Sua sequência de diários atual é **${user.Daily.CurrentStreak}**.`,
+		descriptionReceived: (userLastDaily: string) => `Você já recebeu sua grana diária nas últimas 24 horas.\n-# Poderá receber novamente ${userLastDaily}!`,
+		description: (money: number, currentStreak: number) => `Você recebeu ${formatMoney(money, Language.Portuguese)}.\n-# Sua sequência de diários atual é **${currentStreak}**.`,
 		footer: (maxStreak: number) => `Sequência máxima: ${maxStreak}`,
 	},
 	[Language.Spanish]: {
-		descriptionReceived: (nickname: string, userLastDaily: string) => `**${nickname}** ya recibió su dinero diario en las últimas 24 horas.\n-# Podrá recibirla nuevamente ${userLastDaily}!`,
-		description: (user: User, money: number) => `**${user.Nickname}** recibió ${formatMoney(money, user.Language)}.\n-# Tu racha diaria actual es **${user.Daily.CurrentStreak}**.`,
+		descriptionReceived: (userLastDaily: string) => `Ya has recibido tu dinero diario en las últimas 24 horas.\n-# Podrá recibirla nuevamente ${userLastDaily}!`,
+		description: (money: number, currentStreak: number) => `Usted recibió ${formatMoney(money, Language.Spanish)}.\n-# Tu racha diaria actual es **${currentStreak}**.`,
 		footer: (maxStreak: number) => `Racha máxima: ${maxStreak}`,
 	},
 } as const;
