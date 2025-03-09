0267d0cfa988a0f35439eb3e93f63c7782b7d2ae
Eduardo Jacobi <dudu.jacobi@gmail.com>
2025-03-09 20:16:45 -0300

    feat: setmoney command

---
+++ commands/admin/setmoney.ts
@@ -0,0 +1,98 @@
+﻿import {
+	ChatInputCommandInteraction,
+	Locale,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
+	SlashCommandIntegerOption,
+	SlashCommandStringOption,
+} from "discord.js";
+import { defaultEmbed, formatMoney } from "../../utils/ui";
+import { checkUser, replyInteraction, sendPrivateMessage } from "../../utils/logic";
+import { User } from "../../models/User";
+import { CrColors } from "../../utils/colors";
+
+enum SetMoneyConfiguration {
+	Add,
+	Set,
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("setmoney")
+		.setDescription("Set money to an user!")
+		.setDescriptionLocalization(Locale.PortugueseBR, "Adicione dinheiro para um usuário!")
+		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.addStringOption((option: SlashCommandStringOption) =>
+			option
+				.setName("userid")
+				.setDescription("The userId who will receive the money")
+				.setDescriptionLocalization(Locale.PortugueseBR, "O ID do usuário que vai receber o dinheiro")
+				.setRequired(true),
+		)
+		.addIntegerOption((option: SlashCommandIntegerOption) =>
+			option
+				.setName("money")
+				.setDescription("How many money")
+				.setDescriptionLocalization(Locale.PortugueseBR, "Quanto de dinheiro")
+				.setRequired(true),
+		)
+		.addIntegerOption((option: SlashCommandIntegerOption) =>
+			option
+				.setName("add_or_set")
+				.setDescription("Add or Set the money")
+				.setDescriptionLocalization(Locale.PortugueseBR, "Adicionar ou definir o dinheiro")
+				.setRequired(true)
+				.addChoices([
+					{
+						name: "Add",
+						value: SetMoneyConfiguration.Add,
+					},
+					{
+						name: "Set",
+						value: SetMoneyConfiguration.Set,
+					},
+				]),
+		),
+
+	async execute(interaction: ChatInputCommandInteraction, user: User) {
+
+		const userId = interaction.options.getString("userid", true);
+		const money = interaction.options.getInteger("money", true);
+		const addOrSet = interaction.options.getInteger("add_or_set", true);
+
+		const target = await checkUser(userId, interaction);
+
+		if (!target) {
+			return await interaction.reply("Didn't find this user");
+		}
+
+		if (addOrSet === SetMoneyConfiguration.Add) {
+			target.Money += money;
+			await sendPrivateMessage(userId, `You received ${formatMoney(money, user.Language)}.`, CrColors.Admin);
+
+			await replyInteraction(interaction, {
+				embeds: [defaultEmbed({
+					nickname: user.Nickname,
+					color: CrColors.Admin,
+					interaction: interaction,
+					description: `${formatMoney(money, user.Language)} added to user ${target.Nickname}`,
+				})],
+			});
+		}
+		else {
+			target.Money = money;
+			await sendPrivateMessage(userId, `Your money is now ${formatMoney(money, user.Language)}.`, CrColors.Admin);
+
+			await replyInteraction(interaction, {
+				embeds: [defaultEmbed({
+					nickname: user.Nickname,
+					color: CrColors.Admin,
+					interaction: interaction,
+					description: `User ${target.Nickname} now has ${formatMoney(money, user.Language)}`,
+				})],
+			});
+		}
+
+		await target.Update();
+	},
+};
