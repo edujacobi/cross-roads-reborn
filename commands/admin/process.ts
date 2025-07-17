d5589d95b540c88fcae933b17ca056052e35dc41
Eduardo Jacobi <dudu.jacobi@gmail.com>
2025-07-17 11:42:51 -0300

    feat: improve /process command

--- commands/admin/process.ts
+++ commands/admin/process.ts
@@ -1,11 +1,20 @@
-﻿import { ChatInputCommandInteraction, Locale, PermissionFlagsBits, SlashCommandBuilder } from "discord.js";
-import { CustomEmbedBuilder } from "../../models/CustomEmbedBuilder";
+﻿import {
+	ChatInputCommandInteraction,
+	Locale,
+	MessageFlags,
+	PermissionFlagsBits,
+	SlashCommandBuilder,
+} from "discord.js";
 import { getClient } from "../../client";
 import { replyInteraction } from "../../utils/logic";
 import { CrColors } from "../../utils/colors";
 import { Users } from "../../database/Users";
 import { Op } from "sequelize";
 import { subMinutes } from "date-fns";
+import { CustomContainerBuilder } from "../../ui/builders/CustomContainerBuilder";
+import { User } from "../../models/User";
+import { EmoteString } from "../../utils/emotes";
+import { Language } from "../../models/Language";
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -14,7 +23,8 @@ module.exports = {
 		.setDescriptionLocalization(Locale.PortugueseBR, "Veja dados do processo Node")
 		.setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
-	async execute(interaction: ChatInputCommandInteraction) {
+	async execute(interaction: ChatInputCommandInteraction, user: User, language: Language) {
+		const s = Strings[language];
 
 		const client = getClient();
 
@@ -27,40 +37,48 @@ module.exports = {
 		});
 
 		const onlineUsers = client.userLastCommand.filter(time => new Date(time) > subMinutes(new Date(), 15)).size;
+		const uptime = client.uptime ? Math.floor(client.uptime / 1000 / 60) : 0;
+		const memoryUsage = process.memoryUsage().heapUsed / 1024 / 1024; // Convert to MB
+		const availableMemory = process.availableMemory() / 1024 / 1024; // Convert to MB
 
-		const embed = new CustomEmbedBuilder()
-			.setColor(CrColors.Admin)
-			.setThumbnail(client.user?.avatarURL() ?? null)
-			.setDescription(`# \`${client.uptime ? Math.floor(client.uptime / 1000 / 60) : 0} min\`
--# Uptime
-# \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\`
--# Memory usage
-# \`${playerCount} (${onlineUsers} online)\`
--# Active players`);
+		const container = new CustomContainerBuilder()
+			.setAccentColor(CrColors.Admin)
+			.setUser(user)
+			.addSectionComponents(section => section
+				.setThumbnailAccessory(thumb => thumb
+					.setURL(client.user?.avatarURL() ?? ""),
+				)
+				.addTextDisplayComponents(text => text
+					.setContent([
+						`-# ${s.uptime}`,
+						`# ${uptime} min`,
+						`-# ${s.memoryUsage}`,
+						`# ${memoryUsage.toFixed(1)} MB / ${availableMemory.toFixed(1)} MB`,
+						`-# ${s.activePlayers}`,
+						`# ${playerCount} (${EmoteString.Online}${onlineUsers} online)`,
+					].join("\n")),
+				),
+			)
+			.addFooter();
 
-		// .setFields([
-		// 	{
-		// 		name: "Uptime",
-		// 		value: `\`${client.uptime ? Math.floor(client.uptime / 1000 / 60) : 0} min\``,
-		// 		inline: true,
-		// 	},
-		// 	{
-		// 		name: "Memory usage",
-		// 		value: `\`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\``,
-		// 		inline: true,
-		// 	},
-		// 	{
-		// 		name: "Online",
-		// 		value: `\`${client.userLastCommand.size}\``,
-		// 		inline: true,
-		// 	},
-		// 	{
-		// 		name: "Players",
-		// 		value: `\`${playerCount}\``,
-		// 		inline: true,
-		// 	},
-		// ]);
+		await replyInteraction(interaction, { components: [container], flags: MessageFlags.IsComponentsV2 });
+	},
+};
 
-		await replyInteraction(interaction, { embeds: [embed] });
+const Strings = {
+	[Language.English]: {
+		uptime: "Uptime",
+		memoryUsage: "Memory usage",
+		activePlayers: "Active players",
+	},
+	[Language.Portuguese]: {
+		uptime: "Tempo de atividade",
+		memoryUsage: "Uso de memória",
+		activePlayers: "Jogadores ativos",
+	},
+	[Language.Spanish]: {
+		uptime: "Tiempo de actividad",
+		memoryUsage: "Uso de memoria",
+		activePlayers: "Jugadores activos",
 	},
 };
