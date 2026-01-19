95cb18bf8a8348233455c0848cf76ee2ba6f8f9c
Eduardo Jacobi <dudu.jacobi@gmail.com>
2026-01-19 09:08:47 -0300

    chore: update casino text

--- commands/utility/casino.ts
+++ commands/utility/casino.ts
@@ -1,6 +1,5 @@
 ﻿import { ChatInputCommandInteraction, Locale, SlashCommandBuilder } from "discord.js";
 import { User } from "../../models/User";
-import { Casino } from "../../models/Casino";
 import { CustomContainerBuilder } from "../../ui/builders/CustomContainerBuilder";
 import { replyWithContainer } from "../../utils/logic";
 import { CrColors } from "../../utils/colors";
@@ -34,12 +33,13 @@ module.exports = {
 			.addTexts([
 				`### ${EmoteString.Heads} ${s.headsTailsTitle}`,
 				s.headTailsDescription,
+				`-# ${s.headTailsDescription2}`,
 			])
 			.addLargeSeparator()
 			.addTexts([
 				`### 🏇 ${s.horseRaceTitle}`,
 				s.horseRaceDescription,
-				s.horseRaceDescription2,
+				`-# ${s.horseRaceDescription2}`,
 			])
 			.addFooter();
 
@@ -54,6 +54,7 @@ const Strings = {
 		description2: `Here you can bet and lose all your money!`,
 		headsTailsTitle: `Heads or Tails`,
 		headTailsDescription: `Bet an amount on a coin that must fall on the same side that you choose. You have a 50% chance of winning. If you win, you get 1.5x the amount bet!`,
+		headTailsDescription2: `Use \`/bet <side> <value>\` to bet on heads or tails.`,
 		horseRaceTitle: `Horse Racing`,
 		horseRaceDescription: `Bet on one of the horses in a race. Races are held every 6 hours. If your horse wins, you share the prize pool with other winners. Maximum bet is based on your ${EmoteString.Attack}ATK.`,
 		horseRaceDescription2: `Use \`/horserace\` to see the next race and to place your bet.`,
@@ -64,6 +65,7 @@ const Strings = {
 		description2: `Aqui você pode apostar e perder todo seu dinheiro!`,
 		headsTailsTitle: `Cara ou Coroa`,
 		headTailsDescription: `Aposte um valor em uma moeda que deve cair no mesmo lado que você escolheu. Você tem 50% de chance de vencer. Se vencer, ganha 1.5x o valor apostado!`,
+		headTailsDescription2: `Use \`/bet <lado> <valor>\` para apostar em cara ou coroa.`,
 		horseRaceTitle: `Corrida de Cavalos`,
 		horseRaceDescription: `Aposte em um dos cavalos em uma corrida. As corridas acontecem a cada 6 horas. Se seu cavalo vencer, você divide o prêmio com outros vencedores. A aposta máxima é baseada no seu ${EmoteString.Attack}ATK.`,
 		horseRaceDescription2: `Use \`/corridadecavalos\` para ver a próxima corrida e para fazer sua aposta.`,
@@ -74,6 +76,7 @@ const Strings = {
 		description2: `¡Aquí puedes apostar y perder todo tu dinero!`,
 		headsTailsTitle: `Cara o Cruz`,
 		headTailsDescription: `Apostar una cantidad en una moneda que debe caer del mismo lado que elijas. Tienes un 50% de posibilidades de ganar. ¡Si ganas, obtienes 1.5 veces la cantidad apostada!`,
+		headTailsDescription2: `Usa \`/bet <lado> <valor>\` para apostar en cara o cruz.`,
 		horseRaceTitle: `Carrera de Caballos`,
 		horseRaceDescription: `Apuesta a uno de los caballos en una carrera. Las carreras se celebran cada 6 horas. Si tu caballo gana, compartes el premio con otros ganadores. La apuesta máxima se basa en tu ${EmoteString.Attack}ATK.`,
 		horseRaceDescription2: `Usa \`/horserace\` para ver la próxima carrera y para hacer tu apuesta.`,
