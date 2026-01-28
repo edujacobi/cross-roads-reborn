df3cae92801adecb3bbf31ef29e15e1765e82596
Eduardo Jacobi <dudu.jacobi@gmail.com>
2026-01-28 10:57:04 -0300

    fix: specialshop not consuming specialcoins in vip

--- commands/utility/specialshop.ts
+++ commands/utility/specialshop.ts
@@ -310,6 +310,7 @@ module.exports = {
 					return replyWithContainer(interaction, container);
 				}
 
+				user.SpecialCoin -= price;
 				await user.AddVip(vipMonths * 30);
 
 				container = addHeader()
