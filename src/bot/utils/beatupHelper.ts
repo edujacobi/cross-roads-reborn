b1d5168ad28df01499797ee425e5ee8b3b43ee9b
Eduardo Jacobi <dudu.jacobi@gmail.com>
2026-06-01 09:49:25 -0300

    fix: grenade message not updating defender info (beatup)

--- src/bot/utils/beatupHelper.ts
+++ src/bot/utils/beatupHelper.ts
@@ -71,7 +71,10 @@ export async function runUserBeatUp(interaction: ChatInputCommandInteraction, be
 				// Time out, do nothing
 			}
 
-			await attacker.GetInfo();
+			await Promise.all([
+				attacker.GetInfo(),
+				defender.GetInfo(),
+			]);
 
 			const availability = await beatUp.CanBeatUser();
 
